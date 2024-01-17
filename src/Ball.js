05778aa77b27f2d3dce7d0d8cd85e9487b6bac78
romanpoudel <romanpoudel325@gmail.com>
2024-01-17 12:53:16 +0545

    added comment for clarity

--- src/Ball.js
+++ src/Ball.js
@@ -1,3 +1,49 @@
+
+// /**
+//  * @fileoverview This file defines the Ball class. A Ball is a circle that can move in a random direction.
+//  */
+// class Ball {
+// 	constructor(x, y, radius, color,direction) {
+// 		this.x = x;
+// 		this.y = y;
+// 		this.radius = radius;
+// 		this.color = color;
+// 		this.direction = direction;
+// 	}
+
+// 	draw(ctx) {
+// 		ctx.beginPath();
+// 		ctx.fillStyle = this.color;
+// 		ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, true);
+// 		ctx.fill();
+// 	}
+//     // move(){
+//     //     this.x +=generateRandomNumberBetween(-1,1)*BALL_SPEED;
+//     //     this.y +=generateRandomNumberBetween(-1,1)*BALL_SPEED;
+// 	// 	if (this.x - this.radius < 0 || this.x + this.radius > CANVAS_WIDTH) {
+// 	// 		this.direction.dx = -this.direction.dx;
+// 	// 	}
+
+// 	// 	if (this.y - this.radius < 0 || this.y + this.radius > CANVAS_HEIGHT) {
+// 	// 		this.direction.dy = -this.direction.dy;
+// 	// 	}
+//     // }
+
+
+// 	/**
+// 	 *
+// 	 * @param {object} otherBall
+// 	 * @returns boolean
+// 	 */
+// 	checkCollision(otherBall) {
+//         const dx = this.x - otherBall.x;
+//         const dy = this.y - otherBall.y;
+//         const distance = Math.sqrt(dx * dx + dy * dy);
+//         return distance < this.radius + otherBall.radius;
+//     }
+// }
+
+//modified the code I submitted, analyzed and tested why and how to handle when two balls spawn in the same point,why previously resulted in a bug where the balls would get stuck in the same point (after )
 class Ball {
 	constructor(x, y, radius, color, dx, dy) {
 		this.x = x;
